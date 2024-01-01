aa6b085ae913305ef8b47ccbb2e192e316916865
Llimy1 <alsgur990104@gmail.com>
2024-01-01 23:06:07 +0900

    #1 - Data Rest 설정

--- board/src/main/java/org/project/board/repository/ArticleCommentRepository.java
+++ board/src/main/java/org/project/board/repository/ArticleCommentRepository.java
@@ -3,9 +3,11 @@ package org.project.board.repository;
 import org.project.board.domain.Article;
 import org.project.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
     List<ArticleComment> findAllByArticle(Article article);
 }
