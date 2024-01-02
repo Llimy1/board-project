15da909f973b7b35cc1fa091b7ab263b1727b53a
Llimy1 <alsgur990104@gmail.com>
2024-01-02 23:48:11 +0900

    #15 - 게시글 관련 뷰 테스트 작성

--- board/src/main/java/org/project/board/controller/ArticleController.java
+++ board/src/main/java/org/project/board/controller/ArticleController.java
@@ -1,2 +1,10 @@
-package org.project.board.controller;public class ArticleController {
+package org.project.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
 }
