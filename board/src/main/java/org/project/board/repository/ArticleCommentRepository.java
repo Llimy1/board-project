e39a895ccc8b6ab1c0caa42ad18fc068974c366a
Llimy1 <alsgur990104@gmail.com>
2024-01-02 22:51:35 +0900

    #1 - querydsl 적용

--- board/src/main/java/org/project/board/repository/ArticleCommentRepository.java
+++ board/src/main/java/org/project/board/repository/ArticleCommentRepository.java
@@ -1,13 +1,27 @@
 package org.project.board.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.project.board.domain.Article;
 import org.project.board.domain.ArticleComment;
+import org.project.board.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-    List<ArticleComment> findAllByArticle(Article article);
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createAt, root.createBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+    }
 }
